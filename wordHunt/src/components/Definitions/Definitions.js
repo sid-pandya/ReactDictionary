f1e44c0be035aefd9f1d6d5062b73249f670c6aa
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2021-10-30 16:23:37 -0700

    final

---
+++ wordHunt/src/components/Definitions/Definitions.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Definitions.css';
+
+const Definitions = ({word, category, meanings, LightMode }) => {
+    return (
+        <div className="meanings">
+            {
+                meanings[0] && word && category === "en" && (
+                    <audio 
+                        src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} 
+                        style={{ background:"#fff", borderRadius: 10}}
+                        controls    
+                    >
+                        Your Browser doesn't support audio.
+                    </audio>
+
+                )
+            }
+            
+           {word === "" ? (
+               <span className="subTitle">Start by typing a word in Search</span>
+           ) : (
+            meanings.map((mean) => 
+                mean.meanings.map((item) => (
+                    item.definitions.map((def) => (
+                        <div className='singleMean' 
+                        style={{backgroundColor: LightMode ? "#282c34":"white", color: LightMode ? "white" : "black"}}
+                        >
+                            <b>{def.definition}</b>
+                            <hr style={{ backgroundColor:"black",width:"100%"}}/>
+                            {
+                                def.example && (
+                                    <span>
+                                        <b>Example:</b>
+                                        {def.example}
+                                    </span>
+                                )
+                            }
+                            {
+                                def.synonyms && (
+                                    <span>
+                                        <b>Synonyms:</b>
+                                        {def.synonyms.map((s) => `${s},`)}
+                                    </span>
+                                )
+                            }
+                        </div>
+                    ))
+
+
+                )))
+           )}
+        </div>
+    );
+};
+
+export default Definitions;
---
+++ wordHunt/src/components/Header/Header.js
@@ -0,0 +1,60 @@
+import { createTheme, MenuItem, TextField, ThemeProvider } from '@material-ui/core';
+import React from 'react'
+import './Header.css';
+import categories from "../../data/category";
+
+const Header = ({setCategory, category,word,setWord, LightMode}) => {
+
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main:LightMode ? '#000': '#fff',
+            },
+          mode: LightMode ? 'light':'dark',
+        },
+      });
+
+    const handleChange=(language) => {
+        setCategory(language);
+        setWord("");
+    };  
+
+    
+    return (
+        <div className='header'> 
+            <span className='title'>{word?word : "Word Hunt"}</span>
+            <div className="inputs">
+                
+
+                <ThemeProvider theme={darkTheme}> 
+                    <TextField 
+                        className="search" 
+                        label="Search a Word" 
+                        value={word}
+                        onChange={(e)=>setWord(e.target.value)}
+                    />
+                    <TextField className="select"
+                        select
+                        label="Language"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}
+                        variant="standard"
+                        >
+                            {
+                                categories.map((option) => (
+                                <MenuItem key={option.label} value={option.label} >
+                                    {option.value}
+                                </MenuItem>
+                                ))}
+                  
+                    </TextField>
+
+                </ThemeProvider>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
